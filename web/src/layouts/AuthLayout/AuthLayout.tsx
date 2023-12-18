95a7daa708c0aef855399769f0345c6623cc7ce9
GemboJones <gembojones@gmail.com>
2023-12-18 20:01:15 +0000

    adds footer component

--- web/src/layouts/AuthLayout/AuthLayout.tsx
+++ web/src/layouts/AuthLayout/AuthLayout.tsx
@@ -1,13 +1,18 @@
+import Footer from "src/components/Footer/Footer"
+
 type AuthLayoutProps = {
   children?: React.ReactNode
 }
 
 const AuthLayout = ({ children }: AuthLayoutProps) => {
   return (
-    <main className="min-w-screen min-h-screen bg-auth bg-silverTree dark:bg-nileBlue bg-[center_top] bg-no-repeat bg-[length:1440px_auto] min[1440px]:bg-[length:100%_auto]">
-      <img src="/images/logo__secret-santa.svg" alt="secret santa" className="mx-auto mb-24 w-[460px] pt-24"/>
+    <>
+      <main className="min-w-screen min-h-screen bg-auth bg-silverTree dark:bg-nileBlue bg-[center_top] bg-no-repeat bg-[length:1440px_auto] min[1440px]:bg-[length:100%_auto]">
+        <img src="/images/logo__secret-santa.svg" alt="secret santa" className="mx-auto mb-24 w-[460px] pt-24"/>
       {children}
-    </main>
+      </main>
+    <Footer />
+    </>
   )
 }
 
